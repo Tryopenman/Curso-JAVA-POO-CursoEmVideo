6fdba537c6d84ae5f2ac55fe3cfcdd1ff57b6a8f
Francisco Osório <118193667+Tryopenman@users.noreply.github.com>
2023-01-31 18:29:42 +0000

    Aula03

---
+++ Aula03/src/Aula03/Caneta.java
@@ -0,0 +1,31 @@
+package Aula03;
+
+public class Caneta {
+    public String modelo;
+    public String cor;
+    private float ponta;
+    protected int carga;
+    private boolean tampada;
+    public void status(){
+        System.out.println("Modelo: " + this.modelo);
+        System.out.println("Cor: " + this.cor);
+        System.out.println("Ponta: " + this.ponta);
+        System.out.println("Carga: " + this.carga);
+        System.out.println("Está tampada? " + this.tampada);
+    }
+    public void rabiscar(){
+
+        if (this.tampada == true){
+            System.out.println("Erro! Não posso rabiscar");
+        }else {
+            System.out.println("Estou a rabiscar");
+        }
+    }
+    public void tampar(){
+        this.tampada = true;
+    }
+
+    public void destampar(){
+        this.tampada = false;
+    }
+}
